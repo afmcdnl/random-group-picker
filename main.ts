44138cfdbfc77a0fe5deee36f2cf14b46b348e63
Andrew <afmcdnl@gmail.com>
2020-08-05 10:11:00 -0400

    Changing the random to include a list.

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
 input.onGesture(Gesture.Shake, function () {
-    basic.showNumber(randint(1, 3))
+    let list: number[] = []
+    basic.showString("" + (text_list.removeAt(randint(0, list.length - 1))))
 })
+let text_list: string[] = []
+text_list = ["Fun with French", "Helping the Elderly", "The Whale Radar", "Alarm system"]
