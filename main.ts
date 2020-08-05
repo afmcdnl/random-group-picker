35480c1a33a081ae2357a8fda1679f795bee2988
Andrew <afmcdnl@gmail.com>
2020-08-05 10:39:22 -0400

    Update the array length

--- main.ts
+++ main.ts
@@ -1,6 +1,5 @@
 input.onGesture(Gesture.Shake, function () {
-    let list: number[] = []
-    basic.showString("" + (text_list.removeAt(randint(0, list.length - 1))))
+    basic.showString("" + (text_list.removeAt(randint(0, text_list.length - 1))))
 })
 let text_list: string[] = []
 text_list = ["Fun with French", "Helping the Elderly", "The Whale Radar", "Alarm system"]
