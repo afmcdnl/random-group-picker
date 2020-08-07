12d0dcd7e0b1291a6f226e72ca71136d9867b7e6
Andrew <afmcdnl@gmail.com>
2020-08-07 10:23:36 -0400

    Updated the speed of the string

--- main.ts
+++ main.ts
@@ -1,5 +1,9 @@
+/**
+ * basic.showString("Hello!", 50)
+ */
+// This project is to help us pick the final presentation order
 input.onGesture(Gesture.Shake, function () {
-    basic.showString("" + (text_list.removeAt(randint(0, text_list.length - 1))))
+    basic.showString("" + text_list.removeAt(randint(0, text_list.length - 1)),50)
 })
-let text_list: string[] = []
+let text_list : string[] = []
 text_list = ["Fun with French", "Helping the Elderly", "The Whale Radar", "Alarm system"]
