0db997af7c2c2b7e6eac2523b6117750ca0af5cf
Andrew <afmcdnl@gmail.com>
2020-08-19 10:07:50 -0400

    Added another list of options.

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-/**
- * basic.showString("Hello!", 50)
- */
 // This project is to help us pick the final presentation order
 input.onGesture(Gesture.Shake, function () {
     basic.showString("" + text_list.removeAt(randint(0, text_list.length - 1)),50)
 })
+/**
+ * basic.showString("Hello!", 50)
+ */
 let text_list : string[] = []
-text_list = ["Fun with French", "Helping the Elderly", "The Whale Radar", "Alarm system"]
+// text_list = ["Fun with French", "Helping the Elderly", "The Whale Radar", "Alarm system"]
+text_list = ["Social", "Enviro", "Teacher", "Driving"]
